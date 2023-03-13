185542d91f0badb26f369267e9036411ca087e9f
tdoan1213 <126900958+tdoan1213@users.noreply.github.com>
2023-03-13 11:29:23 -0700

    first push

---
+++ My Game/settings.py
@@ -0,0 +1,22 @@
+#File created by Tim Doan
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings
+FPS = 30
+RUNNING = True
+
+# player attributes
+PLAYER_ACC = 2
+PLAYER_FRICTION = 0.12
+
+# mob attributes
+MOB_ACC = 0.5
+MOB_FRICTION = 0.03
+
+# colors
+BLACK = (0,0,0)
+BLUE = (50,50,255)
+RED = (255,50,50)
