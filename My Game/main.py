9748f0824482d82363285296923cacd41150f1ea
tdoan1213 <126900958+tdoan1213@users.noreply.github.com>
2023-04-06 14:51:43 -0700

    FINAL SUBMISSION
    added walls, changed colors, removed mobs

--- My Game/main.py
+++ My Game/main.py
@@ -1,12 +1,4 @@
 # File created by: Tim Doan
-# Agenda:
-# gIT GITHUB    
-# Build file and folder structures
-# Create libraries
-# testing github changes
-# I changed something - I changed something else tooooo!
-
-# This file was created by: Chris Cozort
 # Sources: http://kidscancode.org/blog/2016/08/pygame_1-1_getting-started/
 # Sources: 
 '''
